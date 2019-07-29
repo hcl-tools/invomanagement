ee3695f66e8e5ab44a75f6fc96744e1512651374
crazywolf132 <crazywolf132@gmail.com>
2019-07-29 14:18:03 +1000

    Added business pojo

---
+++ ordermicroservice/src/main/java/com/ordermicroservice/bean/Buisness.java
@@ -0,0 +1,43 @@
+package com.ordermicroservice.bean;
+
+public class Buisness {
+
+    private int id;
+    private String title;
+    private String address;
+
+
+    public Buisness() {
+    }
+
+    public Buisness(int id, String title, String address) {
+        this.id = id;
+        this.title = title;
+        this.address = address;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
