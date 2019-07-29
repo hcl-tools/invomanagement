6ba539dafefb3c8e06b4d4f26c2761cd617fdaff
esearle9 <emma.searle@hcl.com>
2019-07-29 13:44:54 +1000

    oops

---
+++ itemmicroservice/src/main/java/com/itemmicroservice/ItemmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.itemmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemmicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemmicroserviceApplication.class, args);
+    }
+
+}
