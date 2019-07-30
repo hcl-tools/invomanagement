d2a6a9524eaa23681a38e6270ea3275813df9c70
esearle9 <emma.searle@hcl.com>
2019-07-30 10:28:10 +1000

    updated
    controller and repo

--- itemmicroservice/src/main/java/com/dao/ItemRepo.java
+++ itemmicroservice/src/main/java/com/dao/ItemRepo.java
@@ -2,8 +2,17 @@ package com.dao;
 
 import com.bean.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ItemRepo extends JpaRepository<Item, Integer> {
 
+    @Query(value = "SELECT i FROM Item i WHERE i.sell", nativeQuery = false)
+    List<Item> findSoldItems();
+
+    @Query(value = "SELECT i FROM Item i WHERE i.buy", nativeQuery = false)
+    List<Item> findBoughtItems();
 
 }
